a8f3e5f9a55f30b05c027a5a1b608e9efdf93378
Wylie Fisher <wylie@dukeofcheese.com>
2018-04-27 09:19:42 -0400

    first major commit 🏆

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/home';
+
+import './_index.css';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
 registerServiceWorker();
