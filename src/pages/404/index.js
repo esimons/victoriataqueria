765abf66fa069c760200e67faa60ac71b24ce32c
Evan Simons <esimons@ebsco.com>
2018-04-27 22:09:04 -0400

    Converting over to a Gatsby static-site. Stuff is probably broken!

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Content from '../content';
+import Content from '../../components/content';
 
 import './_index.css';
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import Content from '../content';
+import Content from '../components/content';
 
-import Image from '../../images/lavictoria.jpg';
+import Image from '../images/lavictoria.jpg';
 
 import './_index.css';
 
