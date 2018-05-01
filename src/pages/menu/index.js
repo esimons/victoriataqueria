1ae9177e4a5130cc5118d644c8f210dc03ce805b
Evan Simons <evan.simons@gmail.com>
2018-05-01 17:30:46 -0400

    Bringing up to date with Wylie's improvements (#2)
    Bringing up to date with Wylie's improvements.

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,62 +1,203 @@
 import React from "react";
 
 import Content from "../../components/content";
+import Heading from "../../components/heading";
+
+// import luchadore from "../../images/luchadore.png";
 
 import "./_index.css";
 
 const Menu = () => {
   return (
-    <Content>
-      <div>
-        <p className="Menu_paragraph">
-          Burrito 5.5* (12” Flour Tortilla) Mexican Rice / Beans / Pico De Gallo
-          / Choice Of Filling
-        </p>
-        <p className="Menu_paragraph">
-          Quesadilla 4.95* (10” Flour Tortilla) Cheese / Choice Of Filling
-        </p>
-        <p className="Menu_paragraph">
-          Taco 2.85 Soft Corn Tortilla / Onions / Cilantro / Salsa / Choice Of
-          Filling
-        </p>
-        <p className="Menu_paragraph">
-          Plato Mexicano 5.95* Mexican Rice / Beans / Pico De Gallo / Sour Cream
-          / Choice Of Filling
-        </p>
-        <p className="Menu_paragraph">
-          Salad 3.95 Lettuce / Tomatoes / Avacado / Onions / Queso Fresco
-        </p>
-        <p className="Menu_paragraph">
-          *Add Guacamole, Sour Cream Or Cheese .50 Each Extra Fillings Add 1.00
-          Each
-        </p>
-        <p className="Menu_paragraph">
-          Tortas 6.50 (Mexican Sandwich) Telera Bread / Bean Puree / Tomato /
-          Jalapeños / Onions / Queso Fresco ‘ Chipotle Mayo / Avacado / Lettucs*
-        </p>
-        <p className="Menu_paragraph">Victoria Smoked Ham / Pork Carnitas</p>
-        <p className="Menu_paragraph">Milanesa * Breaded Chicken Fillet</p>
-        <p className="Menu_paragraph">
-          Choriqueso Mexican Sausage / Melted Cheese
-        </p>
-        <p className="Menu_paragraph">
-          Dos Caidas Pork Carnitas / Mexican Sausage
-        </p>
-        <p className="Menu_paragraph">El Chavo * Smoked Ham</p>
-        <p className="Menu_paragraph">Vegetal * Assorted Vegetables</p>
-        <p className="Menu_paragraph">Surtido Pork Skin / Meat</p>
-        <p className="Menu_paragraph">Al Pastor Marinated Pork</p>
-        <p className="Menu_paragraph">
-          Fillings Marinated Chicken Pork Carnitas Mexican Sausage Steak Pastor
-          Vegetables Pork Skin / Meat Beef Tongue
-        </p>
-        <p className="Menu_paragraph">
-          Sides Rice 2 Beans 2 Chips & Salsa 2.5 Guacamole 2.75
-        </p>
-        <p className="Menu_paragraph">
-          Drinks Horchata 2 Jarritos 2 Sodas 1.5 Water 1.5
-        </p>
-      </div>
+    <Content className="Menu">
+      <ul className="Menu_list">
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Burrito 5.5*
+          </Heading>
+          <p className="Menu_subHeading">(12” Flour Tortilla)</p>
+          <ul className="Menu_subList">
+            <li>Mexican Rice</li>
+            <li>Beans</li>
+            <li>Pico De Gallo</li>
+            <li>Choice Of Filling</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Quesadilla 4.95*
+          </Heading>
+          <p className="Menu_subHeading">(10” Flour Tortilla)</p>
+          <ul className="Menu_subList">
+            <li>Cheese</li>
+            <li>Choice Of Filling</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Taco 2.85
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Soft Corn Tortilla</li>
+            <li>Onions</li>
+            <li>Cilantro</li>
+            <li>Salsa</li>
+            <li>Choice Of Filling</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Plato Mexicano 5.95*
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Mexican Rice</li>
+            <li>Beans</li>
+            <li>Pico De Gallo</li>
+            <li>Sour Cream</li>
+            <li>Choice Of Filling</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Salad 3.95
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Lettuce</li>
+            <li>Tomatoes</li>
+            <li>Avacado</li>
+            <li>Onions</li>
+            <li>Queso Fresco</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <p className="Menu_note">
+            *Add Guacamole, Sour Cream Or Cheese .50 Each. Extra Fillings Add
+            1.00 Each
+          </p>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Tortas 6.50
+          </Heading>
+          <p className="Menu_subHeading">(Mexican Sandwich)</p>
+          <ul className="Menu_subList">
+            <li>Telera Bread</li>
+            <li>Bean Puree</li>
+            <li>Tomato</li>
+            <li>Jalapeños</li>
+            <li>Onions</li>
+            <li>Queso Fresco ‘ Chipotle Mayo</li>
+            <li>Avacado</li>
+            <li>Lettucs*</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Victoria
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Smoked Ham</li>
+            <li>Pork Carnitas</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Milanesa *
+          </Heading>
+          <p className="Menu_subHeading">Breaded Chicken Fillet</p>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Choriqueso
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Mexican Sausage</li>
+            <li>Melted Cheese</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Dos Caidas
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Pork Carnitas</li>
+            <li>Mexican Sausage</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            El Chavo *
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Smoked Ham</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Vegetal *
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Assorted Vegetables</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Surtido
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Pork Skin</li>
+            <li>Meat</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Al Pastor
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Marinated Pork</li>
+          </ul>
+        </li>
+      </ul>
+      <ul className="Menu__secondary_list">
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Fillings
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Marinated Chicken</li>
+            <li>Pork Carnitas</li>
+            <li>Mexican Sausage</li>
+            <li>Steak</li>
+            <li>Pastor</li>
+            <li>Vegetables</li>
+            <li>Pork Skin</li>
+            <li>Meat</li>
+            <li>Beef Tongue</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Sides
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Rice 2</li>
+            <li>Beans 2</li>
+            <li>Chips &amp; Salsa 2.5</li>
+            <li>Guacamole 2.75</li>
+          </ul>
+        </li>
+        <li className="Menu_item">
+          <Heading className="Menu_heading" level="2">
+            Drinks
+          </Heading>
+          <ul className="Menu_subList">
+            <li>Horchata 2</li>
+            <li>Jarritos 2</li>
+            <li>Sodas 1.5</li>
+            <li>Water 1.5</li>
+          </ul>
+        </li>
+      </ul>
     </Content>
   );
 };
