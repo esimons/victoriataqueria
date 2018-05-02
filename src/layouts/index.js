d9a1d6f53c5cdacb88f1714d8f49232a36e67aba
Evan Simons <evan.simons@gmail.com>
2018-05-01 21:59:52 -0400

    Adding page title and meta tags. (#4)

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Helmet from "react-helmet";
 import Navbar from "../components/navbar";
 
 import "./_index.css";
 
 const Page = ({ children }) => {
-  return (
-    <div>
+  return <div>
+      <Helmet>
+        <html lang="en" />
+        <title>La Victoria</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+      </Helmet>
       <Navbar />
       {children()}
-    </div>
-  );
+    </div>;
 };
 
 export default Page;
